270aeeddf16c8300847397c8c79dc74539dc2a67
mehrabhossain1 <mehrab.munna00@gmail.com>
2024-03-30 03:18:08 +0600

    28-10 Create Top Rated Doctors Section With Data Fetching

--- src/components/ui/HomePage/Specialist/Specialist.tsx
+++ src/components/ui/HomePage/Specialist/Specialist.tsx
@@ -15,7 +15,7 @@ const Specialist = async () => {
   });
   const { data: specialties } = await res.json();
 
-  console.log(specialties);
+  //   console.log(specialties);
   return (
     <Container>
       <Box
