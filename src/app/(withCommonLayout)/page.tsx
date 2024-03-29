8156bb86f8abc1e4746ca07ab5e77c2332fe5073
mehrabhossain1 <mehrab.munna00@gmail.com>
2024-03-30 02:16:38 +0600

    28-7 Fetch Specialties Data And Create Specialties Section

--- src/app/(withCommonLayout)/page.tsx
+++ src/app/(withCommonLayout)/page.tsx
@@ -1,10 +1,12 @@
 import HeroSection from "@/components/ui/HomePage/HeroSection/HeroSection";
+import Specialist from "@/components/ui/HomePage/Specialist/Specialist";
 import { Container } from "@mui/material";
 
 const HomePage = () => {
   return (
     <>
       <HeroSection />
+      <Specialist />
     </>
   );
 };
