203cc4f2e496d67ea5a22606e7642e2125d61265
mehrabhossain1 <mehrab.munna00@gmail.com>
2024-04-24 19:22:23 +0600

    33-5 Implementing An API Endpoint For Creating Specialties

--- src/app/(withDashboardLayout)/dashboard/admin/specialties/components/SpecialistModal.tsx
+++ src/app/(withDashboardLayout)/dashboard/admin/specialties/components/SpecialistModal.tsx
@@ -2,6 +2,7 @@ import PHFileUploader from "@/components/Forms/PHFileUploader";
 import PHForm from "@/components/Forms/PHForm";
 import PHInput from "@/components/Forms/PHInput";
 import PHModal from "@/components/shared/PHModal/PHModal";
+import { modifyPayload } from "@/utils/modifyPayload";
 import { Button, Grid, TextField } from "@mui/material";
 import React from "react";
 import { FieldValues } from "react-hook-form";
@@ -12,7 +13,16 @@ type TProps = {
 };
 
 const SpecialistModal = ({ open, setOpen }: TProps) => {
-  const handleFormSubmit = (values: FieldValues) => {};
+  const handleFormSubmit = (values: FieldValues) => {
+    console.log(values);
+
+    const data = modifyPayload(values);
+
+    try {
+    } catch (err: any) {
+      console.error(err.message);
+    }
+  };
 
   return (
     <PHModal open={open} setOpen={setOpen} title="Create A New Specialty">
